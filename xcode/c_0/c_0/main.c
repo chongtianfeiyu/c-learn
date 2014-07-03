877a785ab00cc642d30f6e00a35602143a53225b
laizhihuan <kojavaee@qq.com>
2014-07-03 15:03:45 +0800

    Initial Commit

--- xcode/c_0/c_0/main.c
+++ xcode/c_0/c_0/main.c
@@ -1,5 +1,5 @@
 //
-//  main.c
+//  c语言程序入口，main.c
 //  c_0
 //
 //  Created by zhihuanglai on 14-7-3.
@@ -8,11 +8,19 @@
 
 #include <stdio.h>
 
+//argc 命令行输入的参数大小
+//argv 命令行输入的参数数组
 int main(int argc, const char * argv[])
 {
-
+    
+    for(int i=0; i<argc; i++) {
+        printf("%s\n",argv[i]);
+    }
+    
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Hello, C!\n");
+    
+    //程序退出的错误的代码
     return 0;
 }
 
